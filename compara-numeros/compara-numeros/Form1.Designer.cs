7f1b93292bd9032254527b8a937ffeefeca2d753
Murilo Eugenio <117952903+MuriloOGenio@users.noreply.github.com>
2023-11-24 20:04:16 -0300

    Add files via upload

---
+++ compara-numeros/compara-numeros/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace compara_numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnComparar = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.lblResultado.Location = new System.Drawing.Point(329, 318);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(70, 76);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "0";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(167, 124);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(456, 124);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(100, 20);
+            this.txtNum3.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(319, 124);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // btnComparar
+            // 
+            this.btnComparar.Location = new System.Drawing.Point(324, 196);
+            this.btnComparar.Name = "btnComparar";
+            this.btnComparar.Size = new System.Drawing.Size(75, 23);
+            this.btnComparar.TabIndex = 4;
+            this.btnComparar.Text = "Comparar";
+            this.btnComparar.UseVisualStyleBackColor = true;
+            this.btnComparar.Click += new System.EventHandler(this.btnComparar_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.lblMensagem.Location = new System.Drawing.Point(188, 248);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(352, 46);
+            this.lblMensagem.TabIndex = 5;
+            this.lblMensagem.Text = "O maior número é:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnComparar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnComparar;
+        private System.Windows.Forms.Label lblMensagem;
+    }
+}
+
---
+++ compara-numeros/compara-numeros/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace compara_numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int num3 = int.Parse(txtNum3.Text);
+
+
+            //if (num1 >= num2 && num1 >= num3)
+            //{
+            //    lblResultado.Text = num1.ToString();
+            //}
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+            //    lblResultado.Text = num2.ToString();
+            //}
+            //else
+            //{
+            //    lblResultado.Text = num3.ToString();
+            //}
+
+            int[] numeros = { num1, num2, num3 };
+
+            lblResultado.Text = numeros.Max().ToString();
+
+
+
+        }
+    }
+}
